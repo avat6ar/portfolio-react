047925c0461e2e2c645a2e037dd380b61f13205f
AvaT6aR <abdullah2662888@gmail.com>
2023-02-07 05:48:28 +0200

    React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
+import "./App.css";
+import About from "./Components/About/About";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Project from "./Components/Project/Project";
+import Contact from "./Components/Contact/Contact";
 
-function App() {
+const App = () => {
+  const [dark, setDark] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <Header />
+        <Routes>
+          <Route path="portfolio-react/" element={<Home />} />
+          <Route path="portfolio-react/about" element={<About />} />
+          <Route path="portfolio-react/projects" element={<Project />} />
+          <Route path="portfolio-react/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/Components/Project/api/arrProjects.js
@@ -0,0 +1,128 @@
+import ecommerc from "./images_project/commerc.png";
+import curd from "./images_project/curd.png";
+import classyads from "./images_project/classyads.png";
+import pizza from "./images_project/pizza.png";
+import quiz from "./images_project/quiz.png";
+import software from "./images_project/software.png";
+import calculator from "./images_project/calculator.png";
+import audio from "./images_project/audio.png";
+import clock from "./images_project/clock.png";
+import french from "./images_project/french.png";
+import agency from "./images_project/agency.png";
+import apex from "./images_project/apex.png";
+import html from "./images_project/html.png";
+import css from "./images_project/css.png";
+import bootstrap from "./images_project/bootstrap.png";
+import js from "./images_project/js.png";
+import api from "./images_project/api.png";
+
+export const arrProjects = [
+  {
+    id: 1,
+    img: ecommerc,
+    title: "E-commerc",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    link_github: "https://github.com/avat6ar/ecommerc",
+    link: "https://avat6ar.github.io/ecommerc/",
+    languages: [html,css,bootstrap,js],
+  },
+  {
+    id: 2,
+    img: curd,
+    title: "Crud System",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    link_github: "https://github.com/avat6ar/crud-system",
+    link: "https://avat6ar.github.io/crud-system/",
+    languages: [html,css,bootstrap],
+  },
+  {
+    id: 3,
+    img: classyads,
+    title: "Classyads",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    link_github: "https://github.com/avat6ar/classyads",
+    link: "https://avat6ar.github.io/classyads/",
+    languages: [html,css,bootstrap],
+  },
+  {
+    id: 4,
+    img: pizza,
+    title: "Api Pizza",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    link_github: "https://github.com/avat6ar/Api-Pizza",
+    link: "https://avat6ar.github.io/Api-Pizza/",
+    languages: [api,html,css,bootstrap,js],
+  },
+  {
+    id: 5,
+    img: quiz,
+    title: "Quiz App",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    link_github: "https://github.com/avat6ar/QuizApp",
+    link: "https://avat6ar.github.io/QuizApp/",
+    languages: [html,css,js ],
+  },
+  {
+    id: 6,
+    img: software,
+    title: "Software",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    link_github: "https://github.com/avat6ar/software",
+    link: "https://avat6ar.github.io/software/",
+    languages: [html,css,bootstrap],
+  },
+  {
+    id: 7,
+    img: calculator,
+    title: "Calculator App",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    link_github: "https://github.com/avat6ar/calculator",
+    link: "https://avat6ar.github.io/calculator/",
+    languages: [html,css,js],
+  },
+  {
+    id: 8,
+    img: audio,
+    title: "Audio Player App",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    link_github: "https://github.com/avat6ar/audio-player",
+    link: "https://avat6ar.github.io/audio-player/",
+    languages: [html,css,js],
+  },
+  {
+    id: 9,
+    img: clock,
+    title: "Wall Clock App",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    link_github: "https://github.com/avat6ar/wall_clock",
+    link: "https://avat6ar.github.io/wall_clock/",
+    languages: [html,css,js],
+  },
+  {
+    id: 10,
+    img: french,
+    title: "French Restaurant",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    link_github: "https://github.com/avat6ar/french-restaurant",
+    link: "https://avat6ar.github.io/french-restaurant/",
+    languages: [html,css],
+  },
+  {
+    id: 11,
+    img: agency,
+    title: "Engage Agency",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    link_github: "https://github.com/avat6ar/agency",
+    link: "https://avat6ar.github.io/agency/",
+    languages: [html,css],
+  },
+  {
+    id: 12,
+    img: apex,
+    title: "Apex",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    link_github: "https://github.com/avat6ar/apex",
+    link: "https://avat6ar.github.io/apex/",
+    languages: [html,css],
+  },
+];
